d4f6127b1bc50b48d3a8bb53178cef6799df9bf0
rose534 <wambuirose534@gmail.com>
2023-03-29 12:10:19 +0300

    css

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,8 +1,18 @@
+import React from 'react';
 
 function Category(){
     return(
         <div>
-
+            <div id="sidebar">
+                <h3>Category</h3>
+                <div className="category-names">
+                    <p>All</p>
+                    <p>Kitchen</p>
+                    <p>Food</p>
+                    <p>Electronics</p>
+                    <p>Fashion</p>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,15 +1,27 @@
+import Category from "./Category";
+
 function Homepage() {
     return (
         <div>
       <div id="container">
-        <div id="box1"></div>
+        <div id="box1">
+          <p id="welcome">Welcome back!</p>
+        </div>
         <div id="image-container">
           <img src="https://53525363.000webhostapp.com/Images/Download_premium_png_of_Sofa_png_mockup_in_green_fabric_on_transparent_background_about_sofa__sofa_png__armchair__couch__and_furniture_3101307-removebg-preview.png" alt="" className="homeimage"/>
+          
+        </div>
+        
+        <div>
+
         </div>
         <div id="box2"></div>
       </div>
-      <div id="box3">
-
+      {/* <h2 id="list">LIST OF ITEMS</h2> */}
+      <Category/>
+      
+      <div>
+        
       </div>
       </div>
     );
