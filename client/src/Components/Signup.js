4617bb77590aa637c4dd3924264560887532f3d5
sheema <sheemashamilfarook@gmail.com>
2023-03-29 12:06:55 +0300

    Login and signup page done

---
+++ client/src/Components/Signup.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submitting', email, password);
+    // Here you can send the email and password to your server to create a new user
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
