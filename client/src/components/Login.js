99a4fe3cbc62c501fdc23570e94e496ab53d0e9e
rose534 <wambuirose534@gmail.com>
2023-03-31 20:30:20 +0300

    login and signg up page

---
+++ client/src/components/Login.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: handle login logic
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+export default Login;
---
+++ client/src/components/Signup.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function Signup() {
+  const[FirstName, setFirstName] = useState('');
+  const[LastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submitting', email, password);
+    
+  };
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
+export default Signup;
