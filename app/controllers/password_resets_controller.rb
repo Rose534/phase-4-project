bb5b3157687ef399d76a3b6b22d0381df6ec687d
Levy <kipkoechlevy096@gmail.com>
2023-04-01 15:48:56 +0300

    corrected the password reset functionality

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,40 +1,12 @@
 class PasswordResetsController < ApplicationController
-  def new
-  end
-
-  def create
-    user = User.find_by_email(params[:email])
-    if user
-      token = SecureRandom.urlsafe_base64
-      expires_at = 1.hour.from_now
-      user.password_resets.create(token: token, expires_at: expires_at)
-      redirect_to '/login', notice: 'We have sent you an email with instructions on how to reset your password.'
+  
+  def reset_password
+    user = User.find_by(username: params[:username])
+    if user.nil?
+      render json: { message: 'User not found' }, status: :not_found
     else
-      render 'Email not found'
-      
+      user.update(password: params[:password])
+      render json: user, status: :created
     end
-  end
-
-  def edit
-    @user = PasswordReset.find_by_token(params[:token])&.user
-    unless @user
-      redirect_to '/login', alert: 'Invalid or expired token'
-    end
-  end
-
-  def update
-    @user = PasswordReset.find_by_token(params[:token])&.user
-    if @user && @user.update(password_params)
-      @user.password_resets.destroy_all
-      redirect_to '/login', notice: 'Your password was reset successfully. Please sign in.'
-    else
-      render 'edit'
-    end
-  end
-
-  private
-
-  def password_params
-    params.require(:user).permit(:password, :password_confirmation)
   end
 end
