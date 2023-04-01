0c0e6ddd3b5a2f283179a99dfc36322c82fb064b
Levy <kipkoechlevy096@gmail.com>
2023-04-01 15:27:06 +0300

    corrected the crud in products

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :products
+    
     has_many :password_resets, dependent: :destroy
 
     validates :username, { 
--- db/migrate/20230328080544_create_products.rb
+++ db/migrate/20230328080544_create_products.rb
@@ -5,7 +5,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
       t.string :description
       t.integer :price
       t.string :image
-      # t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :user, foreign_key: true
       t.timestamps
     end
   end
