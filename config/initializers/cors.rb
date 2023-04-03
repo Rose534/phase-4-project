acc75d746356e103394cb73f9ae73ac709ff1507
rose534 <wambuirose534@gmail.com>
2023-04-03 06:44:33 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://localhost:4000"
+      origins "https://phase-4-project-ten.vercel.app/"
   
       resource "*",
         headers: :any,
